5e33576c9d41107e8e2dc1038a0daf8d6d2da541
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-09-29 17:41:07 +0300

    create Megfelelonevu method

---
+++ src/Varos.java
@@ -0,0 +1,31 @@
+public class Varos {
+    private String nev;
+    private int lakossag;
+
+    public Varos() {
+    }
+
+    public Varos(String nev, int lakossag) {
+        this.nev = nev;
+        this.lakossag = lakossag;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public int getLakossag() {
+        return lakossag;
+    }
+
+    public void setLakossag(int lakossag) {
+        this.lakossag = lakossag;
+    }
+    public void Megfelelonev(){
+
+    }
+}
